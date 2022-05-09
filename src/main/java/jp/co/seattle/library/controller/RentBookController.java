4fe2197c64f8eca47024dd76bd18eb820869f7f3
kotomi ono <kotomi.ono@seattleconsulting.co.jp>
2022-05-09 17:48:46 +0900

    タスク9の編集

---
+++ src/main/java/jp/co/seattle/library/controller/RentBookController.java
@@ -0,0 +1,51 @@
+package jp.co.seattle.library.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.co.seattle.library.dto.BookDetailsInfo;
+import jp.co.seattle.library.service.BooksService;
+import jp.co.seattle.library.service.RentService;
+
+@Controller // APIの入り口
+/*
+ * レンタルコントローラー
+ */
+public class RentBookController {
+	final static Logger logger = LoggerFactory.getLogger(RentBookController.class);
+
+	@Autowired
+	private RentService rentservice;
+
+	@Autowired
+	private BooksService booksService;
+
+	@RequestMapping(value = "/rentBook", method = RequestMethod.POST) // value＝actionで指定したパラメータ
+	public String deleteBook(Locale locale, @RequestParam("bookId") Integer bookId, Model model) {
+		logger.info("Welcome rent! The client locale is {}.", locale);
+		int rentId = rentservice.rentBook(bookId);
+		BookDetailsInfo bookInfo = new BookDetailsInfo();
+
+		bookInfo.setBookId(bookId);
+
+		if (rentId == 0) {
+			rentservice.rentalBook(bookId);
+			model.addAttribute("bookDetailsInfo", booksService.getBookInfo(bookId));
+
+		} else {
+			model.addAttribute("error", "貸し出し済みです。");
+			model.addAttribute("bookDetailsInfo", booksService.getBookInfo(bookId));
+		}
+
+		return "details";
+	}
+
+}
---
+++ src/main/java/jp/co/seattle/library/service/RentService.java
@@ -0,0 +1,39 @@
+package jp.co.seattle.library.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RentService {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	/**
+     * 書籍IDに紐づく書籍詳細情報を取得する
+     *
+     * @param bookId 書籍ID
+     * @return 書籍情報
+     */
+	public void rentalBook(int bookId) {
+
+		String sql = "INSERT INTO rentalbooks (bookid) VALUES (?);";
+
+		jdbcTemplate.update(sql, bookId);
+
+	}
+	/**
+     * 書籍の貸し出し登録する
+     */
+
+	public int rentBook(int bookId) {
+
+		String sql = "SELECT count (*) FROM rentalbooks where bookid = " + bookId;
+
+		int rentalId = jdbcTemplate.queryForObject(sql, int.class);
+
+		return rentalId;
+
+	}
+
+}
